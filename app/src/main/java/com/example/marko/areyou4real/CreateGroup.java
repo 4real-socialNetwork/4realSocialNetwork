77681f27827d296b4e5efdff65ec8afb134e5b38
Marko Soljic <marko.soljic@gmail.com>
2018-10-24 15:41:16 +0200

    Back button update
    All activities now have a back button on the toolbar.
    Also changed language to croatian.

--- app/src/main/java/com/example/marko/areyou4real/CreateGroup.java
+++ app/src/main/java/com/example/marko/areyou4real/CreateGroup.java
@@ -7,11 +7,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.ImageButton;
 import android.widget.Toast;
+import android.widget.Toolbar;
 
 import com.example.marko.areyou4real.fragments.adapter.UsersAdapter;
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -45,6 +47,7 @@ public class CreateGroup extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_group);
+        setUpToolbar();
 
         mChipsInput = findViewById(R.id.chips_input);
 
@@ -145,4 +148,20 @@ public class CreateGroup extends AppCompatActivity {
         dialog.getWindow().setAttributes(lp);
     }
 
+    private void setUpToolbar (){
+        android.support.v7.widget.Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId()==android.R.id.home){
+            finish();
+        }
+        return true;
+    }
 }
